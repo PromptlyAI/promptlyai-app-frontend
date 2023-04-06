f018e8022d2c11687bf32e73ee2a4caafb4bb183
Wilgot <wilgotwardell@hotmail.com>
2023-04-06 22:14:10 +0200

    words used

--- src/components/upgrade-button/UpgradeButton.tsx
+++ src/components/upgrade-button/UpgradeButton.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UpgradeButton.css";
 export default function UpgradeButton() {
+  const [wordsUsed, setWordsUsed] = useState<number>(5000);
   return (
     <div className="upgrade-container">
       <div className="left-upgrade">
-        <span>5000/10,000 FREE WORDS USED</span>
-        <input
-          className="slider"
-          type="range"
-          min="0"
-          max="100"
-          value="50"
-        ></input>
+        <span>{wordsUsed}/10,000 FREE WORDS USED</span>
+        <div className="words-left-bar-container">
+          <div
+            style={{ width: wordsUsed / 44 }}
+            className="words-left-bar"
+          ></div>
+        </div>
       </div>
       <button className="upgrade">UPGRADE</button>
     </div>
