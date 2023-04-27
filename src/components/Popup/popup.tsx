27a5fe76c67696010c553a58fe84806edf431e0d
Wilgot <wilgotwardell@hotmail.com>
2023-04-27 17:00:11 +0200

    -

--- src/components/Popup/popup.tsx
+++ src/components/Popup/popup.tsx
@@ -9,27 +9,11 @@ export default function Popup(props: PopupProps) {
   const { displayPopup } = props;
   const navigate = useNavigate();
 
-  const popupStyle: CSSProperties = {
-    display: displayPopup ? "flex" : "none",
-    position: "fixed",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    zIndex: 1000,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  };
-
   return (
-    <div style={popupStyle}>
+    <div className="popupStyle">
       <div className="popupContentStyle">
         <h1>Welcome to PromptlyLabs</h1>
-        <h2 style={{ cursor: "auto",
-        color:"#fff"
-        }}>
+        <h2 style={{ cursor: "auto", color: "#fff" }}>
           Log in with your PromptlyLabs account to continue
         </h2>
         <div className="popupButtonContainer">
