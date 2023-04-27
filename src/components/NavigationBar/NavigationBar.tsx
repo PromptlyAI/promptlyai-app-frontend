8743a86ba793e4ad64bfd8469ce88a3c200b13e7
Wilgot <wilgotwardell@hotmail.com>
2023-04-27 17:19:41 +0200

    .

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -8,7 +8,17 @@ import { useNavigate } from "react-router";
 import Api from "../../api/Api";
 import { RxExit } from "react-icons/rx";
 import { SidebarContext } from "../../context/SidebarContext";
-export default function NavigationBar() {
+import SettingsPage from "../../pages/SettingsPage/SettingsPage";
+
+interface IProps {
+  showSettings: boolean;
+  setShowSettings: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function NavigationBar({
+  showSettings,
+  setShowSettings,
+}: IProps) {
   const { showSidebar, setShowSidebar } = useContext(SidebarContext);
 
   const [userName, setUserName] = useState<string>("");
@@ -29,42 +39,48 @@ export default function NavigationBar() {
   }, []);
 
   return (
-    <div className="navigation-bar-main-container">
-      <div
-        onClick={() => setRotate(!rotate)}
-        className={rotate ? "menu-btn rotate" : "menu-btn"}
-      >
-        <img src={MenuBtn} alt="" />
-      </div>
-      <div className={rotate ? "exit-btn fade-in" : "exit-btn fade-out"}>
-        <RxExit className="exit-icon" />
-      </div>
-      <div className="nav-btn-container">
+    <>
+      {showSettings && <SettingsPage />}
+      <div className="navigation-bar-main-container">
         <div
-          style={{ background: "#605C9D", borderRadius: "13px" }}
-          className="nav-btn"
+          onClick={() => setRotate(!rotate)}
+          className={rotate ? "menu-btn rotate" : "menu-btn"}
         >
-          <img src={Search} alt="" />
+          <img src={MenuBtn} alt="" />
         </div>
-        <div className="nav-btn bottom-border">
-          <img src={ImgBox} alt="" />
+        <div className={rotate ? "exit-btn fade-in" : "exit-btn fade-out"}>
+          <RxExit className="exit-icon" />
         </div>
-        <div
-          style={{
-            background: showSidebar ? "#605C9D" : "transparent",
-            borderRadius: "13px",
-          }}
-          className="nav-btn"
-          onClick={() => setShowSidebar(!showSidebar)}
-        >
-          <img src={Time} alt="" />
+        <div className="nav-btn-container">
+          <div
+            style={{ background: "#605C9D", borderRadius: "13px" }}
+            className="nav-btn"
+          >
+            <img src={Search} alt="" />
+          </div>
+          <div className="nav-btn bottom-border">
+            <img src={ImgBox} alt="" />
+          </div>
+          <div
+            style={{
+              background: showSidebar ? "#605C9D" : "transparent",
+              borderRadius: "13px",
+            }}
+            className="nav-btn"
+            onClick={() => setShowSidebar(!showSidebar)}
+          >
+            <img src={Time} alt="" />
+          </div>
+        </div>
+        <div className="bottom-pfp">
+          <div
+            onClick={() => setShowSettings(!showSettings)}
+            className="profile-picture"
+          >
+            {userName?.charAt(0).toUpperCase()}
+          </div>
         </div>
       </div>
-      <div className="bottom-pfp">
-        <div onClick={() => navigate("/settings")} className="profile-picture">
-          {userName?.charAt(0).toUpperCase()}
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
