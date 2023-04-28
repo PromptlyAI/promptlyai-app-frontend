b45410aa44f153dd77aab73a897fb29684ccd189
21ulwe <21ulwe@stockholmscience.se>
2023-04-28 11:12:28 +0200

    slak

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -44,67 +44,61 @@ export default function LoginPage() {
   return (
     <div className="login-page-container">
       <div className="login-page">
-        <div>
-          <div className="center">
-            <img className="logo" src={Logo} alt="" />
-          </div>
-          <form>
-            <div className="login-inputs">
-              <h1>Login</h1>
-              <div style={{ height: "25px" }}>
-                {loginFailed && (
-                  <span style={{ color: "red" }}>
-                    Invalid email or password
-                  </span>
-                )}
-              </div>
-
-              <div style={{ display: "flex", width: "200px" }}>
-                <label className="login-label" htmlFor="">
-                  Enter email
-                </label>
-              </div>
-
-              <input
-                className="login-input"
-                type="text"
-                value={email}
-                onChange={(ev) => setEmail(ev.target.value)}
-              />
-              <div style={{ display: "flex", width: "200px" }}>
-                <label className="login-label" htmlFor="">
-                  Enter password
-                </label>
-              </div>
-              <input
-                className="login-input"
-                type="password"
-                value={password}
-                onChange={(ev) => setPassword(ev.target.value)}
-              />
-              <button
-                style={{
-                  width: "175px",
-                  height: "40px",
-                }}
-                className="loggin-btn"
-                onClick={(ev) => login(ev)}
-              >
-                {loading ? (
-                  <div className="center">
-                    <div className="loader"></div>
-                  </div>
-                ) : (
-                  <>Login</>
-                )}
-              </button>
-
-              <Link className="register-button" to="/register">
-                Don't have have an account? <br /> Sign up for PromptlyAI
-              </Link>
-            </div>
-          </form>
+        <div className="center">
+          <img className="logo" src={Logo} alt="" />
         </div>
+        <form>
+          <h1>Login</h1>
+          <div style={{ height: "25px" }}>
+            {loginFailed && (
+              <span style={{ color: "red" }}>Invalid email or password</span>
+            )}
+          </div>
+
+          <div style={{ display: "flex", width: "200px" }}>
+            <label className="login-label" htmlFor="">
+              Enter email
+            </label>
+          </div>
+
+          <input
+            className="login-input"
+            type="text"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <div style={{ display: "flex", width: "200px" }}>
+            <label className="login-label" htmlFor="">
+              Enter password
+            </label>
+          </div>
+          <input
+            className="login-input"
+            type="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button
+            style={{
+              width: "175px",
+              height: "40px",
+            }}
+            className="loggin-btn"
+            onClick={(ev) => login(ev)}
+          >
+            {loading ? (
+              <div className="center">
+                <div className="loader"></div>
+              </div>
+            ) : (
+              <>Login</>
+            )}
+          </button>
+
+          <Link className="register-button" to="/register">
+            Don't have have an account? <br /> Sign up for PromptlyAI
+          </Link>
+        </form>
       </div>
     </div>
   );
