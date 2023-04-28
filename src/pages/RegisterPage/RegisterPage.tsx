43a3e112b1ab839e12dbfc1639099fd6c29ec25a
Uljas Wennström <uljas.wennstrom@gmail.com>
2023-04-28 23:52:55 +0200

    register

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -165,7 +165,7 @@ export default function RegisterPage() {
                     <div className="loader"></div>
                   </div>
                 ) : (
-                  <>Login</>
+                  <>Register</>
                 )}
               </button>
             </div>
