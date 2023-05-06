678a0db506d81a45ae7a8360bdfe674836fbc4f0
Uljas Wennström <uljas.wennstrom@gmail.com>
2023-05-06 23:03:09 +0200

    forgot pass

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -42,102 +42,112 @@ export default function LoginPage() {
 
   async function forgotPassword() {}
   return (
-    <div className="login-page-container">
-      <div className="login-page">
-        <div>
-          <img style={{ width: "260px" }} className="logo" src={Logo} alt="" />
-          <form
+  <div className="login-page-container">
+    <div className="login-page">
+      <div>
+        <img style={{ width: "260px" }} className="logo" src={Logo} alt="" />
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div
             style={{
+              width: "80%",
               display: "flex",
               flexDirection: "column",
-              alignItems: "center",
+              gap: "10px",
             }}
           >
+            <h1>Login</h1>
+            {loginFailed && (
+              <div style={{ height: "25px" }}>
+                <span style={{ color: "red" }}>
+                  Invalid email or password
+                </span>
+              </div>
+            )}
+
+            <div style={{ display: "flex", width: "200px" }}>
+              <label className="login-label" htmlFor="">
+                Enter email
+              </label>
+            </div>
             <div
               style={{
-                width: "80%",
+                width: "100%",
                 display: "flex",
-                flexDirection: "column",
-                gap: "10px",
+                justifyContent: "center",
               }}
             >
-              <h1>Login</h1>
-              {loginFailed && (
-                <div style={{ height: "25px" }}>
-                  <span style={{ color: "red" }}>
-                    Invalid email or password
-                  </span>
-                </div>
-              )}
-
-              <div style={{ display: "flex", width: "200px" }}>
-                <label className="login-label" htmlFor="">
-                  Enter email
-                </label>
-              </div>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <input
-                  className="login-input"
-                  type="text"
-                  value={email}
-                  onChange={(ev) => setEmail(ev.target.value)}
-                />
-              </div>
-
-              <div style={{ display: "flex", width: "200px" }}>
-                <label className="login-label" htmlFor="">
-                  Enter password
-                </label>
-              </div>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <input
-                  className="login-input"
-                  type="password"
-                  value={password}
-                  onChange={(ev) => setPassword(ev.target.value)}
-                />
-              </div>
-              <div>
-                <label className="login-label" htmlFor="">
-                  Don’t have an account?{" "}
-                  <Link className="register-button" to="/register">
-                    Sign up
-                  </Link>
-                </label>
-              </div>
-
-              <button
-                style={{
-                  width: "175px",
-                  height: "40px",
-                }}
-                className="loggin-btn"
-                onClick={(ev) => login(ev)}
-              >
-                {loading ? (
-                  <div className="center">
-                    <div className="loader"></div>
-                  </div>
-                ) : (
-                  <>Login</>
-                )}
-              </button>
+              <input
+                className="login-input"
+                type="text"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
             </div>
-          </form>
-        </div>
+
+            <div style={{ display: "flex", width: "200px" }}>
+              <label className="login-label" htmlFor="">
+                Enter password
+              </label>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <input
+                className="login-input"
+                type="password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+            </div>
+
+            <div>
+              <label className="login-label" htmlFor="">
+                Don’t have an account?{" "}
+                <Link className="register-button" to="/register">
+                  Sign up
+                </Link>
+              </label>
+            </div>
+
+            <div>
+              <label className="login-label" htmlFor="">
+                <Link className="register-button" to={`/forgot-password?email=${email}`}>
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+
+            <button
+              style={{
+                width: "175px",
+                height: "40px",
+              }}
+              className="loggin-btn"
+              onClick={(ev) => login(ev)}
+            >
+              {loading ? (
+                <div className="center">
+                  <div className="loader"></div>
+                </div>
+              ) : (
+                <>Login</>
+              )}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-  );
+  </div>
+);
+
 }
