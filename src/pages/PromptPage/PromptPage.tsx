29c33485cb0d08fa69fe9728d48aec5d0bedfe56
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-06-02 20:47:28 +1000

    -

--- src/pages/PromptPage/PromptPage.tsx
+++ src/pages/PromptPage/PromptPage.tsx
@@ -1,9 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PromptTool from "../../components/PromptTool/PromptTool";
 import { AppContext } from "../../context/AppContext";
+import "./PromptPage.css";
+import StyledInput from "../../shared/input-styles/StyledInput";
 
 export default function PromptPage() {
   const { screenDimensions } = useContext(AppContext);
+  const [userInput, setUserInput] = useState<string>("");
   return (
     <div
       style={{
@@ -12,7 +15,73 @@ export default function PromptPage() {
         // marginLeft: screenDimensions.w < 1219 && "200px",
       }}
     >
-      <PromptTool />
+      {/* <PromptTool /> */}
+
+      <div style={{ display: "flex", gap: "20px" }}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100vh",
+              width: "500px",
+              alignItems: "center",
+              background: "orange",
+              gap: "10px",
+            }}
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "50%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <StyledInput
+                inpHeight={"90%"}
+                inpWidht="97%"
+                textSize={screenDimensions.w > 1800 ? "22px" : "15px"}
+                inpStyle={1}
+                title={userInput}
+                change={(ev) => setUserInput(ev.target.value)}
+                placeHolder="Write your prompt input here..."
+              />
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "50%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <StyledInput
+                inpHeight={"90%"}
+                inpWidht="97%"
+                textSize={screenDimensions.w > 1800 ? "22px" : "15px"}
+                inpStyle={1}
+                title={userInput}
+                change={(ev) => setUserInput(ev.target.value)}
+                placeHolder="Write your prompt input here..."
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            width: "500px",
+            alignItems: "center",
+            background: "orange",
+          }}
+        >
+          <div>Settings</div>
+          <div>Actions</div>
+        </div>
+      </div>
     </div>
   );
 }
