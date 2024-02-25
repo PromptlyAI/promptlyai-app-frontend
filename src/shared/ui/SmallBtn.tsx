faea5eed69aa718bb31f5964bb389dbf57f99a3a
Wilgot Wardell <wilgotwardell@hotmail.com>
2024-02-25 14:41:16 +0100

    -

--- src/shared/ui/SmallBtn.tsx
+++ src/shared/ui/SmallBtn.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export default function SmallBtn({ text, func }: IProps) {
   return (
     <button
-      className="w-36 h-12 text-center rounded-md text-black hover:bg-transparent transition-colors bg-grayscale-lgray"
+      className="w-36 h-12 text-center rounded-md text-black hover:bg-transparent hover:text-white hover:border transition-colors bg-grayscale-lgray"
       onClick={func}
     >
       {text}
