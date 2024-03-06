f14c4f989f8438ef44f63d9551edfa4a95c034ca
Wilgot Wardell <wilgotwardell@hotmail.com>
2024-03-06 14:37:03 +0100

    -

--- src/prompt-grad/PromptGrad.tsx
+++ src/prompt-grad/PromptGrad.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./PromptGrad.css";
-import Toolbar from "./shared/Toolbar";
-import GradInput from "./shared/GradInput";
+import Toolbar from "./components/Toolbar";
+import GradInput from "./components/GradInput";
 
 export default function PromptGrad() {
   //älgarna protesterar. älgarna har fått nog. älgarna är arga. älgarna är trötta. älgarna är hungriga. älgarna är ledsna. älgarna är glada. älgarna är förvirrade. älgarna är rädda
